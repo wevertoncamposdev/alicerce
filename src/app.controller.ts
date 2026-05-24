42ccaa1d0fb2be2ee3052c3c4eb1d5d6b4ad20ac
wevertoncamposdev <wcmcampos@hotmail.com>
2026-05-24 19:28:05 -0300

    config swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,7 @@ export class AppController {
   constructor(private readonly appService: AppService) { }
 
   @Public()
-  @Get()
+  @Get("/")
   getHello(): string {
     return this.appService.getHello();
   }
